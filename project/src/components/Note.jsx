784be3a2df09da63fedfb1b6bf532fe0c11fb3e7
subhaLakshmi-S <subha14092000@gmail.com>
2021-06-06 11:17:47 +0530

    firstcommit

---
+++ project/src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This ia an amazing bootcamp taken up by Shaurya Sinha sir.They covered
+        everything from scratch including Javascript,react.js,HTMl.
+      </p>
+    </div>
+  );
+}
+export default Note;
